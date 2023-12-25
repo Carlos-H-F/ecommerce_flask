ecfc284e0ca132c8e5f797ab0d3328c7edaa915f
Carlos-H-F <furtado20092000@gmail.com>
2023-12-25 10:23:55 -0300

    novo_design

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+db.init_app(app)
+
+
 
 @app.route('/')
 def page_home():
-    return "<h3>The King is back</h3>"
+    return render_template("index.html")
+
+@app.route('/sobre')
+def page_home():
+    return render_template("sobre.html")
